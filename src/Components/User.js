7993d11d793098fd7cfa1365b2b603fa13288297
aushvin vasanthakumaran <aushvin.vasanth@klyck.io>
2020-01-25 16:46:10 -0500

    User

--- src/Components/User.js
+++ src/Components/User.js
@@ -5,19 +5,30 @@ class User extends Component{
   constructor(props) {
     super(props);
     this.state = { 
-        dayList: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
+        dayList: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'],
+        userInfo: {}
     };
   }
 
   render() {
-    return this.state.dayList.map((day)=> {
+
+    const calender = this.state.dayList.map((day)=> {
         return (
             <div>
                 <h3>{day}</h3>
                 <input></input>
             </div>
         )
-    })
+    });
+    return (
+        <div>
+            <h3>User-Id</h3>
+            <input></input>
+            <div>{calender}</div>
+            <button>Submit Entry</button>
+        </div>
+    )
+
   }
 
 }
