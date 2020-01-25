eeefbfa8350d8140e7462b8815db3b389a7982ed
aushvin vasanthakumaran <aushvin.vasanth@klyck.io>
2020-01-25 18:32:29 -0500

    Fixed localhost backend

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class User extends Component{
 
@@ -6,13 +7,48 @@ class User extends Component{
     super(props);
     this.state = { 
         dayList: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'],
-        userInfo: {}
+        userInfo: {"username":"Aushvin Vasanth", "priority": "6", "timeRequest": [7,1,6,2,5,3,4]},
+        userId: String,
+        Monday: String,
+        Tuesday: String,
+        
     };
   }
 
+  componentDidMount() {
+    Axios({
+      method: 'get',
+      url: `http://localhost:5000/api/v1/employee/all`,
+    }).then((res) => {
+        console.log(res);
+    })}
+
+    submit = () => {
+        Axios({
+            method:'post',
+            url:'http://localhost:5000/api/v1/employee/all',
+            data: this.state.userInfo,
+        }).then((res)=> {
+            console.log(res)
+        })
+    }
+
+    handleChangeUserInput = (event) => {
+        this.setState({
+          userId: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+    event.preventDefault();
+    }
+  
+
+
+
   render() {
 
-    const calender = this.state.dayList.map((day)=> {
+    const calender = this.state.dayList.map((day, key)=> {
         return (
             <div>
                 <h3>{day}</h3>
@@ -23,9 +59,16 @@ class User extends Component{
     return (
         <div>
             <h3>User-Id</h3>
-            <input></input>
+            <input 
+                onChange={this.handleChangeUserInput}
+                value={this.state.userId}
+                name="userInput"
+                type="text"
+                placeholder="Type in your User ID"
+             />
+            <button onClick={this.handleSubmit}>Submit</button>
             <div>{calender}</div>
-            <button>Submit Entry</button>
+            <button onClick={this.submit}>Submit Entry</button>
         </div>
     )
 
--- src/Components/services/backendService.py
+++ src/Components/services/backendService.py
@@ -1,27 +1,82 @@
 import flask
 from flask import request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+
+cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 
 # Create some test data for our catalog in the form of a list of dictionaries.
-employeeList= []
+
+
+employeeList = [
+    {
+        "username": "A",
+        "priority": "1",
+        "timeRequest": [
+            1,2,3,4,5,6,7
+        ]
+    },
+    {
+        "username": "B",
+        "priority": "2",
+        "timeRequest": [
+            7,6,5,4,3,2,1
+        ]
+    },
+    {
+        "username": "C",
+        "priority": "3",
+        "timeRequest": [
+            4,3,2,1,5,6,7
+        ]
+    },
+    {
+        "username": "D",
+        "priority": "4",
+        "timeRequest": [
+            4,6,7,1,3,2,5
+        ]
+    },
+    {
+        "username": "E",
+        "priority": "5",
+        "timeRequest": [
+            6,7,1,5,4,3,2
+        ]
+    },
+    {
+        "username": "F",
+        "priority": "6",
+        "timeRequest": [
+            7,1,6,2,5,3,4
+        ]
+    },
+    {
+        "username": "G",
+        "priority": "7",
+        "timeRequest": [
+            1,2,3,4,5,7,6
+        ]
+    }
+    ]
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-        <p>A prototype API for distant reading of science fiction novels.</p>'''
+    return '''<h1>Employee List Archive</h1>
+              <p>A prototype API for Employee List - Delta Hacks</p>'''
 
-
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+@app.route('/api/v1/employee/all', methods=['GET'])
 def api_all():
     return jsonify(employeeList)
 
-
-
-@app.route('/api/v1/resources/books/all', methods=['POST'])
-def create_player():
-    #employeeList = []
+# curl -i -H "Content-Type: application/json" -X POST -d '{"username":"Aushvin Vasanth"}' http://localhost:5000/api/v1/employee/all
+@app.route('/api/v1/employee/all', methods=['POST'])
+def create_employee():
     employee = {
         "username": request.json.get('username',''),
         "priority": request.json.get('priority',''),
